cfd7d109dc558e0ececcea0387b89f309a5a785b
Marcio Reis <marcio.reis@outlook.com>
2019-04-17 10:55:18 +0200

    Update source code

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,8 @@
  * @param fallback
  * @returns {any}
  */
-export * from './ensure/ensure-valid-number';
-export * from './ensure/ensure-valid-email';
-export * from './ensure/ensure-valid-string';
-export * from './ensure/ensure-valid-baud-rate';
-export * from './ensure/ensure-valid-data-bits';
-export * from './ensure/ensure-valid-date';
-export * from './ensure/ensure-valid-stop-bit';
-export * from './ensure/ensure-valid-parity';
-export * from './ensure/ensure-boolean';
+import * as ensure from './ensure'
+
+export {
+    ensure
+}
