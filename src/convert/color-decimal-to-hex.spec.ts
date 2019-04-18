a64e5ae2c22b09b09d9d773e69b43803f97aaecc
Marcio Reis <marcio.reis@outlook.com>
2019-04-18 18:38:42 +0200

    1.0.2

--- src/convert/color-decimal-to-hex.spec.ts
+++ src/convert/color-decimal-to-hex.spec.ts
@@ -1,7 +1,7 @@
-import {colorDecimalToHex} from './color-decimal-to-hex';
+import { colorDecimalToHex } from './color-decimal-to-hex';
 
 test('colorDecimalToHex', () => {
-    expect(colorDecimalToHex(16777215)).toBe('ffffff');
-    expect(colorDecimalToHex(0)).toBe('000000');
-    expect(colorDecimalToHex(255)).toBe('0000ff');
+  expect(colorDecimalToHex(16777215)).toBe('ffffff');
+  expect(colorDecimalToHex(0)).toBe('000000');
+  expect(colorDecimalToHex(255)).toBe('0000ff');
 });
