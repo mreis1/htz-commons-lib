5c1b94adcd03635ce81a5564c0e02e715afe8246
Marcio Reis <marcio.reis@outlook.com>
2019-06-26 14:51:40 +0200

    Fix bug on isNullOrUndefinedOrEmptyStr utility method. instances of objects would return true

--- src/utils/is-null-or-undefined-or-empty-str.ts
+++ src/utils/is-null-or-undefined-or-empty-str.ts
@@ -1,14 +1,12 @@
-import { isNullOrUndefined } from './is-null-or-undefined';
-
 export function isNullOrUndefinedOrEmptyStr(
   str: string,
   options: { trim: boolean } = { trim: true }
 ) {
-  if (isNullOrUndefined(str)) {
-    return true;
-  } else if (typeof str === 'string') {
-    return str.trim() === '';
+  if (typeof str === "string") {
+    return str.trim() === "";
   } else {
-    return true;
+    if (str === null || str === void 0) {
+      return true;
+    }
   }
 }
