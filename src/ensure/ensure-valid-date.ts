d295cea0c2ada2904a5671ad9baff39729160bdc
Marcio Reis <marcio.reis@outlook.com>
2019-06-26 14:57:42 +0200

    Fix linting

--- src/ensure/ensure-valid-date.ts
+++ src/ensure/ensure-valid-date.ts
@@ -1,18 +1,19 @@
-import { Moment } from "moment";
-import { isNullOrUndefinedOrEmptyStr } from "../utils/is-null-or-undefined-or-empty-str";
-import moment = require("moment");
+import { Moment } from 'moment';
+import { isNullOrUndefinedOrEmptyStr } from '../utils';
+import moment from 'moment';
 
 export function ensureValidDate(
   date: any,
   defaultValue = void 0,
-  inputFormat = "YYYY-MM-DD"
+  inputFormat = 'YYYY-MM-DD'
 ): Moment {
   const nullOrUndefinedOrEmpty = isNullOrUndefinedOrEmptyStr(date);
   if (nullOrUndefinedOrEmpty) {
     return defaultValue;
   } else if (date) {
     let m: Moment;
-    if (moment.isMoment(date)) { // Check if is moment object.
+    if (moment.isMoment(date)) {
+      // Check if is moment object.
       m = date;
     } else if (moment.isDate(date)) {
       m = moment(date);
--- src/utils/is-null-or-undefined-or-empty-str.ts
+++ src/utils/is-null-or-undefined-or-empty-str.ts
@@ -2,8 +2,8 @@ export function isNullOrUndefinedOrEmptyStr(
   str: string,
   options: { trim: boolean } = { trim: true }
 ) {
-  if (typeof str === "string") {
-    return str.trim() === "";
+  if (typeof str === 'string') {
+    return str.trim() === '';
   } else {
     if (str === null || str === void 0) {
       return true;
