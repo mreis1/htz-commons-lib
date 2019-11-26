d1fcba5e7d6ac84ce883e6b0f4add9ccc0bbf785
Marcio Reis <marcio.reis@outlook.com>
2019-11-26 14:56:05 +0100

    Add support to decimals in BGR format instead of traditional RGB

--- src/convert/color-decimal-to-hex.spec.ts
+++ src/convert/color-decimal-to-hex.spec.ts
@@ -4,4 +4,5 @@ test('colorDecimalToHex', () => {
   expect(colorDecimalToHex(16777215)).toBe('ffffff');
   expect(colorDecimalToHex(0)).toBe('000000');
   expect(colorDecimalToHex(255)).toBe('0000ff');
+  expect(colorDecimalToHex(255, true)).toBe('ff0000');
 });
--- src/convert/color-decimal-to-hex.ts
+++ src/convert/color-decimal-to-hex.ts
@@ -1,7 +1,41 @@
-export function colorDecimalToHex(color: number) {
+/**
+ * Converts decimal to HEX.
+ * Can handle delphi conversion.
+ *
+ * <b>About delphi conversion:</b>
+ * Delphi stores values as BGR instead of RGB
+ *
+ * For that reason
+ *
+ * b = 0
+ * g = 5
+ * r = 255
+ * to be 1535 (in decimal)
+ *
+ * @param color
+ * @param bgrMode  (Value are stored as BGR instead of RGB)
+ */
+export function colorDecimalToHex(color: number, bgrMode?: boolean) {
   let hex = color.toString(16);
   while (hex.length < 6) {
     hex = '0' + hex;
+  }
+  if (bgrMode) {
+    let hexChar = hex.split('');
+    let hexParts = [];
+    let c = [];
+    while (hexChar.length) {
+      const v = hexChar.shift();
+      if (c.length <= 1) {
+        c.push(v);
+      }
+      if (c.length === 2) {
+        const block = c.join('');
+        hexParts.push(block);
+        c = [];
+      }
+    }
+    hex = hexParts.reverse().join('');
   }
   return hex;
 }
