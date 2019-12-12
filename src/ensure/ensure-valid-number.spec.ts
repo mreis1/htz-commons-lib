b40c829951dbe4b1f7367019f7d2a0a53827c4b4
Marcio Reis <marcio.reis@outlook.com>
2019-12-12 16:31:11 +0100

    Improves ensureValidNumber(). Until now, every string was parsed into integer without having validated the string. For example: parseInt('12a') would return 12 but '12a' is actually an invalid representation of a numeric value. With this implementation, only strings representing valid numbers will be considered. If you want to convert decimals or decimal strings into integers, make sure to provide `{ convertDecimal: true }`

---
+++ src/ensure/ensure-valid-number.spec.ts
@@ -0,0 +1,31 @@
+import { ensureValidNumber } from './ensure-valid-number';
+
+describe('#ensureValidNumber', function() {
+  it('Should return void 0', function() {
+    expect(ensureValidNumber('1.1a')).toBe(void 0);
+    expect(ensureValidNumber('1.11')).toBe(void 0); // Without convertDecimal
+    expect(ensureValidNumber('1.11.11')).toBe(void 0);
+    expect(ensureValidNumber({})).toBe(void 0);
+  });
+
+  it('Should return default value', function() {
+    expect(ensureValidNumber('1.1a', 1)).toBe(1);
+    expect(ensureValidNumber('1.11', 1)).toBe(1); // Without convertDecimal
+    expect(ensureValidNumber('1.11.11', 1)).toBe(1);
+    expect(ensureValidNumber({}, 1)).toBe(1);
+  });
+
+  it('Should parse string of decimals', function() {
+    expect(
+      ensureValidNumber('2.1a', void 0, { convertDecimal: true })).toBe(void 0
+    );
+    expect(ensureValidNumber('2.11', void 0, { convertDecimal: true })).toBe(2);
+    expect(ensureValidNumber('2,11', void 0, { convertDecimal: true })).toBe(2);
+  });
+
+  it('Should parse numbers', function() {
+    expect(ensureValidNumber(2.22, void 0, { convertDecimal: true })).toBe(2);
+    expect(ensureValidNumber(2.22, void 0, { convertDecimal: false })).toBe(void 0);
+    expect(ensureValidNumber(2, void 0, { convertDecimal: true })).toBe(2);
+  });
+});
--- src/ensure/ensure-valid-number.ts
+++ src/ensure/ensure-valid-number.ts
@@ -1,15 +1,51 @@
 /**
- * Takes any value and matches it against a regex.
- * If is a number it returns it
- * If is a numeric string it returns it's numeric version
- * Otherwise returns the fallback that by default is void 0
- *
+ * Takes a string value or a number and matches it against a regex.
+ * It converts the value to a integer.
+ * If conversion succeeds, it will return a integer.
+ * Otherwise returns the fallback that by default is `void 0`
  * @param value
  * @param fallback
+ * @param options
+ * @param options.convertDecimal  {boolean} (false)   Either a decimal string should be converted to integer (Example: '11.11' => 11)
  * @returns {any}
  */
-export function ensureValidNumber(value: any, fallback?) {
+export function ensureValidNumber(value: any, fallback?, options?: { convertDecimal: boolean; }) {
   if (value !== void 0 && value != null) {
+    let t = typeof value;
+    if (t === 'string') {
+      value = value.trim();
+      let isValidString = false;
+      if (options?.convertDecimal) {
+        /**
+         * 11 = true
+         * 11.11 = true
+         * 11.11a = false
+         * 11.11.11 = false
+         * a11.11.11 = false
+         */
+        isValidString = /^\d+([,|.]?\d+)$/g.test(value);
+      } else {
+        /**
+         * 11 = true
+         * 11.11 = false
+         * 11.11a = false
+         * 11.11.11 = false
+         * a11.11.11 = false
+         */
+        isValidString = /^\d+$/g.test(value);
+      }
+      if (!isValidString) {
+        return fallback;
+      }
+    } else if (t === 'number') {
+      if (isNaN(value)) {
+        return fallback;
+      }
+      if (!options?.convertDecimal && (value).toString().indexOf('.') >= 0) {
+        return fallback;
+      }
+    }
+
     const parsed = parseInt(value, 0);
     if (isNaN(parsed)) {
       return fallback;
