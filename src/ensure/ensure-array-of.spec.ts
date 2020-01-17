7050b377d1f0e1d04418b552bf2a772b9a2b241e
Marcio Reis <marcio.reis@outlook.com>
2020-01-17 11:07:25 +0100

    1.2.6

--- src/ensure/ensure-array-of.spec.ts
+++ src/ensure/ensure-array-of.spec.ts
@@ -2,8 +2,8 @@ import { ensureArrayOf } from './ensure-array-of';
 
 describe('ensureArrayOf', function() {
   it('should validate', function() {
-    expect(ensureArrayOf(void 0, (v) => false)).toHaveLength(0);
-    let r1 = ensureArrayOf(['String Value', 1], (v) => {
+    expect(ensureArrayOf(void 0, v => false)).toHaveLength(0);
+    let r1 = ensureArrayOf(['String Value', 1], v => {
       return typeof v === 'number';
     });
     expect(r1[0]).toBe(1);
--- src/ensure/ensure-valid-array.spec.ts
+++ src/ensure/ensure-valid-array.spec.ts
@@ -4,7 +4,7 @@ describe('ensureArray', function() {
   it('should validate', function() {
     expect(ensureValidArray(void 0)).toBe(void 0);
     expect(ensureValidArray(void 0, [])).toHaveLength(0);
-    const a = [1,'a'];
+    const a = [1, 'a'];
     expect(ensureValidArray(a, [])).toBe(a);
   });
 });
