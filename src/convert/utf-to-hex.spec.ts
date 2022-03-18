bdc53dc9b01da4feb6fd44afce34385f2bd826a1
Marcio Reis <marcio.reis@outlook.com>
2022-03-18 17:41:28 +0100

    1.4.0

--- src/convert/utf-to-hex.spec.ts
+++ src/convert/utf-to-hex.spec.ts
@@ -1,13 +1,7 @@
 import { utfToHex } from './utf-to-hex';
 
 describe('utfToHex', () => {
-  test('# Sample test', () =>
-    expect(utfToHex('AA')).toBe(
-      '4141'
-    ))
+  test('# Sample test', () => expect(utfToHex('AA')).toBe('4141'));
   test('# null or undefined should return empty string', () =>
-    expect(utfToHex('AA')).toBe(
-      '4141'
-    ))
+    expect(utfToHex('AA')).toBe('4141'));
 });
-
