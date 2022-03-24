9abe70e6db3f48b407a48c872ae3b711b164ca9f
Marcio Reis <marcio.reis@outlook.com>
2022-03-24 10:51:05 +0100

    1.4.2

--- src/convert/hex-to-buffer.spec.ts
+++ src/convert/hex-to-buffer.spec.ts
@@ -10,9 +10,9 @@ describe('hexToBuffer', () => {
 
   test('should sanitize hex string before converting into buffer', () => {
     let d = hexToBuffer('02:20-10 30');
-    expect(d[0]).toBe(0x02)
+    expect(d[0]).toBe(0x02);
     expect(d[1]).toBe(0x20);
     expect(d[2]).toBe(0x10);
     expect(d[3]).toBe(0x30);
   });
-})
+});
