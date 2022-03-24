122267bd8f95cebae690c246ebd298ee643a90eb
Marcio Reis <marcio.reis@outlook.com>
2022-03-24 10:40:27 +0100

    1.4.1 add unit test

--- src/convert/hex-to-buffer.spec.ts
+++ src/convert/hex-to-buffer.spec.ts
@@ -7,4 +7,12 @@ describe('hexToBuffer', () => {
     expect(() => hexToBuffer('TEST_INVALID_STRING')).toHaveLength(0));
   test('it should return undefined if invalid string', () =>
     expect(hexToBuffer({ some: 'object' })).toBeUndefined());
-});
+
+  test('should sanitize hex string before converting into buffer', () => {
+    let d = hexToBuffer('02:20-10 30');
+    expect(d[0]).toBe(0x02)
+    expect(d[1]).toBe(0x20);
+    expect(d[2]).toBe(0x10);
+    expect(d[3]).toBe(0x30);
+  });
+})
