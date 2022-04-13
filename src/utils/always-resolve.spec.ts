fc4276d434f7ddd696494b73fe8a22c2226cd683
Marcio Reis <marcio.reis@outlook.com>
2022-04-13 11:45:49 +0200

    1.5.0

--- src/utils/always-resolve.spec.ts
+++ src/utils/always-resolve.spec.ts
@@ -1,7 +1,7 @@
 import { alwaysResolve } from './always-resolve';
 
 describe('alwaysResolve', function () {
-     /*
+  /*
           console.log(await alwaysResolve(1, { promiseOnly: true }));
           console.log(await alwaysResolve(() => 1, { promiseOnly: true }));
 
@@ -25,49 +25,49 @@ describe('alwaysResolve', function () {
           }, { promiseOnly: true }))
 
      * */
-     describe('Non promise input values', () => {
-          it('should resolve with INVALID_PROMISE', async function () {
-               let o = await alwaysResolve(1, { promiseOnly: true });
-               expect(o.err?.message).toBe('INVALID_PROMISE');
+  describe('Non promise input values', () => {
+    it('should resolve with INVALID_PROMISE', async function () {
+      let o = await alwaysResolve(1, { promiseOnly: true });
+      expect(o.err?.message).toBe('INVALID_PROMISE');
 
-               o = await alwaysResolve(() => 1, { promiseOnly: true });
-               expect(o.err?.message).toBe('INVALID_PROMISE');
-          });
-          it('should resolve with the non promise value', async function () {
-               let o = await alwaysResolve(1, { promiseOnly: false });
-               expect(o.err).toBe(null);
+      o = await alwaysResolve(() => 1, { promiseOnly: true });
+      expect(o.err?.message).toBe('INVALID_PROMISE');
+    });
+    it('should resolve with the non promise value', async function () {
+      let o = await alwaysResolve(1, { promiseOnly: false });
+      expect(o.err).toBe(null);
 
-               o = await alwaysResolve(1);
-               expect(o.err).toBe(null);
+      o = await alwaysResolve(1);
+      expect(o.err).toBe(null);
 
-               o = await alwaysResolve(() => 1, { promiseOnly: false });
-               expect(o.err).toBe(null);
+      o = await alwaysResolve(() => 1, { promiseOnly: false });
+      expect(o.err).toBe(null);
 
-               o = await alwaysResolve(() => 1);
-               expect(o.err).toBe(null);
-          });
-     });
+      o = await alwaysResolve(() => 1);
+      expect(o.err).toBe(null);
+    });
+  });
 
-     describe('Promise input values', () => {
-          describe('rejected', () => {
-               it('should resolve with true', async function () {
-                    let o = await alwaysResolve(Promise.resolve(true));
-                    expect(o.err).toBe(null);
-                    expect(o.data).toBe(true);
+  describe('Promise input values', () => {
+    describe('rejected', () => {
+      it('should resolve with true', async function () {
+        let o = await alwaysResolve(Promise.resolve(true));
+        expect(o.err).toBe(null);
+        expect(o.data).toBe(true);
 
-                    o = await alwaysResolve(() => Promise.resolve(true));
-                    expect(o.err).toBe(null);
-                    expect(o.data).toBe(true);
-               });
-               it('should resolve with err', async function () {
-                    let o: any = await alwaysResolve(Promise.reject('TEST'));
-                    expect(o.err).toBe('TEST');
-                    expect(o.data).toBe(null);
+        o = await alwaysResolve(() => Promise.resolve(true));
+        expect(o.err).toBe(null);
+        expect(o.data).toBe(true);
+      });
+      it('should resolve with err', async function () {
+        let o: any = await alwaysResolve(Promise.reject('TEST'));
+        expect(o.err).toBe('TEST');
+        expect(o.data).toBe(null);
 
-                    o = await alwaysResolve(() => Promise.reject('TEST'));
-                    expect(o.err).toBe('TEST');
-                    expect(o.data).toBe(null);
-               });
-          });
-     });
+        o = await alwaysResolve(() => Promise.reject('TEST'));
+        expect(o.err).toBe('TEST');
+        expect(o.data).toBe(null);
+      });
+    });
+  });
 });
--- src/utils/always-resolve.ts
+++ src/utils/always-resolve.ts
@@ -1,11 +1,13 @@
 export interface IAlwaysResolveRes<T, Y> {
-     err: any | Y;
-     data: T;
+  err: any | Y;
+  data: T;
 }
 
 type AlwaysResolvePromiseFn<T> = () => Promise<T>;
 type AlwaysResolveNoPromiseFn<T> = () => T;
-export type AlwaysResolvePromiseCall<T> = Promise<T> | AlwaysResolvePromiseFn<T>;
+export type AlwaysResolvePromiseCall<T> =
+  | Promise<T>
+  | AlwaysResolvePromiseFn<T>;
 export type AlwaysResolveNoPromiseCall<T> = T | AlwaysResolveNoPromiseFn<T>;
 /**
  * An utility function that takes a promise and always resolve with a common object structure:
@@ -19,48 +21,56 @@ export type AlwaysResolveNoPromiseCall<T> = T | AlwaysResolveNoPromiseFn<T>;
  *
  */
 export function alwaysResolve<T, Y>(
-     v: AlwaysResolvePromiseCall<T> | AlwaysResolveNoPromiseCall<T>,
-     opts?: {
-          /**
-           * When true, only promises will be accepted.
-           * Passing a function that returns something other than a promise will resolve with data.err
-           * where result.err.message will be `INVALID_PROMISE`.
-           */
-          promiseOnly: boolean;
-     }
+  v: AlwaysResolvePromiseCall<T> | AlwaysResolveNoPromiseCall<T>,
+  opts?: {
+    /**
+     * When true, only promises will be accepted.
+     * Passing a function that returns something other than a promise will resolve with data.err
+     * where result.err.message will be `INVALID_PROMISE`.
+     */
+    promiseOnly: boolean;
+  }
 ): Promise<IAlwaysResolveRes<T, Y>>;
 export function alwaysResolve(
-     v: any,
-     opts?: {
-          /**
-           * When true, only promises will be accepted.
-           * Passing a function that returns something other than a promise will resolve with data.err
-           * where result.err.message will be `INVALID_PROMISE`.
-           */
-          promiseOnly: boolean;
-     }
+  v: any,
+  opts?: {
+    /**
+     * When true, only promises will be accepted.
+     * Passing a function that returns something other than a promise will resolve with data.err
+     * where result.err.message will be `INVALID_PROMISE`.
+     */
+    promiseOnly: boolean;
+  }
 ): any {
-     return new Promise((resolve) => {
-          switch (typeof v) {
-               case 'function':
-                    try {
-                         let o = v();
-                         if (o instanceof Promise) {
-                              o.then((data) => resolve({ err: null, data })).catch((err) => resolve({ err, data: null }));
-                         } else {
-                              !opts?.promiseOnly ? resolve({ err: null, data: o }) : resolve({ err: new TypeError('INVALID_PROMISE'), data: null });
-                         }
-                    } catch (err) {
-                         resolve({ err, data: null });
-                    }
-                    break;
-               case 'object':
-               default:
-                    if (v instanceof Promise) {
-                         v.then((data) => resolve({ err: null, data })).catch((err) => resolve({ err, data: null }));
-                    } else {
-                         !opts?.promiseOnly ? resolve({ err: null, data: v }) : resolve({ err: new TypeError('INVALID_PROMISE'), data: null });
-                    }
+  return new Promise((resolve) => {
+    switch (typeof v) {
+      case 'function':
+        try {
+          let o = v();
+          if (o instanceof Promise) {
+            o.then((data) => resolve({ err: null, data })).catch((err) =>
+              resolve({ err, data: null })
+            );
+          } else {
+            !opts?.promiseOnly
+              ? resolve({ err: null, data: o })
+              : resolve({ err: new TypeError('INVALID_PROMISE'), data: null });
           }
-     });
+        } catch (err) {
+          resolve({ err, data: null });
+        }
+        break;
+      case 'object':
+      default:
+        if (v instanceof Promise) {
+          v.then((data) => resolve({ err: null, data })).catch((err) =>
+            resolve({ err, data: null })
+          );
+        } else {
+          !opts?.promiseOnly
+            ? resolve({ err: null, data: v })
+            : resolve({ err: new TypeError('INVALID_PROMISE'), data: null });
+        }
+    }
+  });
 }
