9c652354adcc8f4226a2efbc44512fb548407356
Marcio Reis <marcio.reis@outlook.com>
2022-04-13 15:27:53 +0200

    additional unit test for `ensureDecimal()`

--- src/ensure/ensure-valid-decimal.spec.ts
+++ src/ensure/ensure-valid-decimal.spec.ts
@@ -1,6 +1,10 @@
 import { ensureValidDecimal } from './ensure-valid-decimal';
 
 describe('ensure-valid-decimal.spec.ts', function () {
+  test('Integer should be accepted', function () {
+    var result = ensureValidDecimal(12);
+    expect(result).toBe(12);
+  });
   test('Handles comma decimals by default', function () {
     var result = ensureValidDecimal('123,23');
     expect(result).toBe(123.23);
