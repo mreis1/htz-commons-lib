53aba11968f57a1dab97b994ee5ff821928ce3f2
Marcio (PRO) <marcio.reis1@hotmail.com>
2024-07-24 10:16:41 +0200

    make the input of elements more flexible by allowing arguments that are arrays of number, number[], buffer, buffer[]

--- src/utils/combine-hex.spec.ts
+++ src/utils/combine-hex.spec.ts
@@ -3,6 +3,39 @@ import { combineHex } from './combine-hex';
 test('combineHex', () => {
   expect(
     combineHex(
+      [Buffer.from('hello'), // Here we use a buffer
+        Buffer.from('test')]
+    ).toString('hex')
+  ).toBe('68656c6c6f74657374');
+
+  expect(
+    combineHex(
+      [
+        Buffer.from('hello'), // Here we use a buffer
+        Buffer.from('test').map(v => v),
+        10
+      ],
+      10
+    ).toString('hex')
+  ).toBe('68656c6c6f746573740a0a');
+
+
+
+  expect(
+    combineHex(
+      [
+        Buffer.from('hello'), // Here we use a buffer
+        [0xff]
+      ], // Here we use an array containing hex values
+      0x10,
+      255,
+      Buffer.from('test')
+    ).toString('hex')
+  ).toBe('68656c6c6fff10ff74657374');
+
+
+  expect(
+    combineHex(
       Buffer.from('hello'), // Here we use a buffer
       [0xff], // Here we use an array containing hex values
       0x10,
@@ -10,6 +43,19 @@ test('combineHex', () => {
       Buffer.from('test')
     ).toString('hex')
   ).toBe('68656c6c6fff10ff74657374');
+
+  expect(
+    combineHex(
+      [
+        Buffer.from('hello'), // Here we use a buffer
+        [0xff]
+      ], // Here we use an array containing hex values
+      0x10,
+      255,
+      Buffer.from('test')
+    ).toString('hex')
+  ).toBe('68656c6c6fff10ff74657374');
+
   expect(combineHex([0xff]).toString('hex')).toBe('ff');
 
   expect(combineHex(0xff).toString('hex')).toBe('ff');
@@ -19,7 +65,7 @@ test('combineHex', () => {
       'hello', // Here we use a buffer
       Buffer.from('test')
     ).toString('hex')
-  ).toThrow('Invalid input value');
+  ).toThrow(/^Invalid input value/g);
 
   expect(combineHex().toString('hex')).toBe('');
 });
--- src/utils/combine-hex.ts
+++ src/utils/combine-hex.ts
@@ -1,25 +1,52 @@
 /**
- * Combines decimal or array of decimals or array of hex values into a buffer
+ * Combines decimal or array of decimals or buffer into a buffer
+ * Strings are not accepted - if you provide a string you will
+ * @Usage
+ *    combineHex([100,20,30], Buffer.from('SOME_UTF8_VALUE'), )
+ *
  * @param args
  */
 export function combineHex(...args) {
   const arr = [];
-  args.forEach((value) => {
+  args.forEach(value => {
     if (value !== void 0 && value != null) {
-      const isBuffer = Buffer.isBuffer(value);
-      const value2 = isBuffer || Array.isArray(value) ? value : [value];
-      if (!isBuffer) {
-        // When a buffer is not provided, only numbers are accepted.
-        for (const v of value2) {
-          if (typeof v !== 'number') {
-            throw new Error('Invalid input value');
-          }
+      const isBuffer = Buffer.isBuffer(value) || value instanceof Buffer;
+      const isArray = Array.isArray(value);
+      const tOfValue = typeof value;
+      if (!isBuffer && !isArray) {
+        if (tOfValue !== 'number') {
+          throw new TypeError(`Invalid input value. Expected number, array or buffer but got ${typeof value}`)
         }
       }
+      const value2 = []
+      if (isBuffer) {
+        value2.push(...value);
+      } else if (isArray) /*Array validation */ {
+        const data = []
+        value.forEach(item => {
+          if (Array.isArray(item)) {
+            if (item.filter(v => typeof v !== 'number').length) {
+              throw new TypeError('Invalid array');
+            } else {
+              data.push(...item);
+            }
+          } else if (Buffer.isBuffer(item)) {
+            data.push(...item)
+          } else {
+            data.push(item)
+          }
+        })
+        value2.push(...data)
+      } else {
+        value2.push(value);
+      }
+
       arr.push.apply(arr, value2);
-    } else {
+    }
+    else {
       throw new Error('Invalid input value');
     }
   });
   return Buffer.from(arr);
 }
+
